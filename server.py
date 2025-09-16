3d6011263242da758232dcf121888393d7d6ad72
YaroslavGmyrya <jjaked25@gmail.com>
2025-09-16 21:21:19 +0700

    Add server for android app

---
+++ server.py
@@ -0,0 +1,29 @@
+import socket
+import json
+
+#define port number
+port = 5000
+
+#create and bind socket
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.bind(("0.0.0.0", port))
+
+#start listen
+my_socket.listen()
+
+location_history = []
+
+#start data transmission
+while True:
+        #wait connection from Kotlin-client
+        client_socket, address = my_socket.accept()
+        print(f"Connection from: {address}")
+
+        while True:
+                #receive data
+                data = client_socket.recv(1024)
+
+                if len(data) != 0:
+                        location_history.append(json.loads(data))
+                        print(f"Received: {data.decode()}")
+
